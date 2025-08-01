80dfce0f099ad60a9d5ed3173ca481f742f1c290
Saurabh Tiwari <158590911+SaurabhCodes-16@users.noreply.github.com>
2025-08-01 13:30:20 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ import numpy as np
 app = FastAPI()
 
 # Set your Gemini API key
-os.environ["GOOGLE_API_KEY"] = "AIzaSyCUnF5Y3iE1O8VMr1UUyJ50Zdwt1N7DDZ4"
+os.environ["GOOGLE_API_KEY"] = ""
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 class HackRxInputSimple(BaseModel):
